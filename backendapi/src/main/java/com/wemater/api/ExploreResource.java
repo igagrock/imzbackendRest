02567ce7f20204f97dcfc7fec587d91ab3884454
Irshad Ahmad Sheikh <irshsheikh@gmail.com>
2015-07-16 04:12:30 +0530

    v4: exploreapi start
    Change-Id: I8dd06d44f733f9370d02a35cb6e1d161cb33f4f6
    Signed-off-by: Irshad Ahmad Sheikh <irshsheikh@gmail.com>

--- backendapi/src/main/java/com/wemater/api/ExploreResource.java
+++ backendapi/src/main/java/com/wemater/api/ExploreResource.java
@@ -1,6 +1,7 @@
 package com.wemater.api;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -10,6 +11,41 @@ import javax.ws.rs.core.MediaType;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class ExploreResource {
+	
+	@GET
+	@Path("/trending")
+	public String getTrendingArticles()
+	{   //eqaual to top stories
+		return "here in trending articles";
+		
+	}
+	@GET
+	@Path("/latest")
+	public String getLatestArticles()
+	{
+		return "here in latest articles";
+		
+	}
+	@GET
+	@Path("/reads")
+	public String getQuickReadArticles()
+	{
+		return "here in quickread articles";
+		
+	}
+	@GET
+	@Path("/explore")
+	public String exploreArticles()
+	{
+		return "here in explore articles";
+		
+	}
+	
+	
+	
+	
+	
+	
 
 }
     
