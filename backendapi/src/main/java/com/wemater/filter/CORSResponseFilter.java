733ac8b74f55bef167e8ce64e23d5a0aa14ff8bc
Irshad Ahmad Sheikh <irshsheikh@gmail.com>
2015-07-31 17:36:38 +0530

    update[added CORS filter/ Updated web.xml]

---
+++ backendapi/src/main/java/com/wemater/filter/CORSResponseFilter.java
@@ -0,0 +1,23 @@
+package com.wemater.filter;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class CORSResponseFilter implements  ContainerResponseFilter{
+
+	@Override
+	public void filter(ContainerRequestContext requestContext,
+			ContainerResponseContext responseContext) throws IOException {
+		System.out.println("CORS FILTER EXECUTED");
+		responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+		responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");	
+		responseContext.getHeaders().add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Wemater");
+		
+	}
+
+}
